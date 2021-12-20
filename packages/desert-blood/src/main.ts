796b5be9323ef4f013e6058b66cc892f8fce6e6b
Ruby Quail <ruby@rubyquail.design>
2021-12-20 16:41:33 +1100

    some splitting out
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- packages/desert-blood/src/main.ts
+++ packages/desert-blood/src/main.ts
@@ -1,9 +1,9 @@
-import "./style.css";
-import * as h from "@rupertofly/h";
+import './style.css';
+import * as h from '@rupertofly/h';
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1>${JSON.stringify(Object.getOwnPropertyNames(h), null, 1)}</h1>
+  <h1>${h.PI}</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
